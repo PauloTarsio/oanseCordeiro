fd5b37ebdf5163c4e772d39a2b25e7926fbe8983
PauloTarsio <paulofortaleza2010@gmail.com>
2025-08-28 15:02:05 -0300

    develop acertando alguns ajustes

---
+++ src/main/resources/db/migration/V20250828_1__adicionando_foto_path_aluno.sql
@@ -0,0 +1,2 @@
+-- Adiciona coluna para caminho da foto do aluno
+ALTER TABLE aluno ADD COLUMN a_foto_path VARCHAR(255);
---
+++ src/main/resources/db/migration/V20250828_2__removendo_foto_path_aluno.sql
@@ -0,0 +1,2 @@
+-- Remove coluna de caminho da foto do aluno
+ALTER TABLE aluno DROP COLUMN a_foto_path;
